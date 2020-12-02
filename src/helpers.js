8a2bba6816d9f64b13af154158d3ee05d903834e
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2020-12-01 22:09:33 -0800

    Fixed broken URL creation

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
   // Creates url with post title and id
   export function createURL(str, title, id) {
-    return `${str[0]}${title.replace(/\s+/g, '-').toLowerCase()}-${id}`;
+    return `${str[0]}${encodeURIComponent(title).replace(/\s+/g, '-').toLowerCase()}-${id}`;
   }
