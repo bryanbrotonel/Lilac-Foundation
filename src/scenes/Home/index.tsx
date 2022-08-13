3b7d77e82da66611dd43cf25805e6e1801d93277
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-08-13 12:56:13 -0700

    Implemented about section layout

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import AboutSection from './AboutSection';
+import BlogSection from './BlogSection';
+import DonateSection from './DonateSection';
 
 function Home() {
   return (
-    <div className="container">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto
-        architecto nesciunt veniam hic quod cum? Ducimus expedita ipsum minus
-        ratione, laborum, iure cupiditate aliquid laudantium eos repellat,
-        molestias maiores quis?
-      </p>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <AboutSection />
+      <BlogSection />
+      <DonateSection />
+    </React.Fragment>
   );
 }
 
