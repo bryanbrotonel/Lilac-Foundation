76b3d8a3f5f5603b114b2aaf9372f0cf7314501e
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-09-08 15:00:40 -0700

    Added code splitting

--- src/navRoutes.tsx
+++ src/navRoutes.tsx
@@ -1,84 +1,31 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Loading from './components/Loading';
-import Home from './scenes/Home';
-import Team from './scenes/Team';
-import About from './scenes/About';
-import Blog from './scenes/Blog';
-import Projects from './scenes/Projects';
-import NotFound from './scenes/NotFound';
-import BlogPost from './scenes/Blog/BlogPost';
-import ProjectPost from './scenes/Projects/ProjectPost';
-import ProfilePage from './scenes/Team/ProfilePage';
+
+const Home = React.lazy(() => import('./scenes/Home'));
+const Team = React.lazy(() => import('./scenes/Team'));
+const About = React.lazy(() => import('./scenes/About'));
+const Blog = React.lazy(() => import('./scenes/Blog'));
+const Projects = React.lazy(() => import('./scenes/Projects'));
+const BlogPost = React.lazy(() => import('./scenes/Blog/BlogPost'));
+const ProjectPost = React.lazy(() => import('./scenes/Projects/ProjectPost'));
+const ProfilePage = React.lazy(() => import('./scenes/Team/ProfilePage'));
+const NotFound = React.lazy(() => import('./scenes/NotFound'));
 
 const NavRoutes = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Home />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="about"
-      element={
-        <Suspense fallback={<Loading />}>
-          <About />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="team"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Team />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="team/:teamMemberSlug"
-      element={
-        <Suspense fallback={<Loading />}>
-          <ProfilePage />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="projects"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Projects />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="projects/:postId"
-      element={
-        <Suspense fallback={<Loading />}>
-          <ProjectPost />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="blog"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Blog />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="blog/:postId"
-      element={
-        <Suspense fallback={<Loading />}>
-          <BlogPost />
-        </Suspense>
-      }
-    ></Route>
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+  <Suspense fallback={<Loading />}>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/team" element={<Team />}></Route>
+      <Route path="team/:teamMemberSlug" element={<ProfilePage />}></Route>
+      <Route path="/blog" element={<Blog />}></Route>
+      <Route path="blog/:postId" element={<BlogPost />}></Route>
+      <Route path="/projects" element={<Projects />}></Route>
+      <Route path="projects/:postId" element={<ProjectPost />}></Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Suspense>
 );
 
 export default NavRoutes;
